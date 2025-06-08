f1e31ae4e82e8392c09d874b6dd9baba3abd3525
AllxdDev(Script Free) <allxddev@gmail.com>
2025-06-08 10:45:56 +0700

    Delete frontend directory

--- frontend/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Modal from './components/Modal.vue'
-import './index.css'
-
-const app = createApp(App)
-app.component('Modal', Modal)
-app.mount('#app')
